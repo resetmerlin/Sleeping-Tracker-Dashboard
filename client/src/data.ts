99870893983e6484f4e12719402379cfde43a3ba
resetmerlin <resetmerlin@gmail.com>
2023-11-08 13:16:08 +0900

    feat: Add dummy data

---
+++ client/src/data.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/prefer-default-export
+export const data = [
+  { date: "2023/10/25", time: 7 },
+  { date: "2023/10/26", time: 7 },
+  { date: "2023/10/27", time: 5 },
+  { date: "2023/10/28", time: 7 },
+  { date: "2023/10/29", time: 6 },
+  { date: "2023/10/30", time: 7 },
+  { date: "2023/10/31", time: 6 },
+  { date: "2023/11/01", time: 6 },
+  { date: "2023/11/02", time: 9 },
+  { date: "2023/11/03", time: 9 },
+  { date: "2023/11/04", time: 6 },
+  { date: "2023/11/05", time: 6 },
+  { date: "2023/11/06", time: 6 },
+  { date: "2023/11/07", time: 5 },
+  { date: "2023/11/08", time: 7 },
+  { date: "2023/11/09", time: 7 },
+];
