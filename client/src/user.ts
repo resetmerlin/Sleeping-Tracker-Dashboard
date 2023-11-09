d7e1beeb749b3325aa908f85c2785ca2410cd668
resetmerlin <resetmerlin@gmail.com>
2023-11-09 19:00:52 +0900

    feat: Add user dummy data

---
+++ client/src/user.ts
@@ -0,0 +1,8 @@
+import { IUser } from "./dto";
+
+// eslint-disable-next-line import/prefer-default-export
+export const user: IUser = {
+  id: 1,
+  username: "john_doe",
+  email: "john.doe@example.com",
+};
