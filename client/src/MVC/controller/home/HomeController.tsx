f8bfcfa2ca09d7f1cff83cca179129b0b4e45b73
resetmerlin <resetmerlin@gmail.com>
2023-11-09 19:01:53 +0900

    feat: Add User, Weather model in Home controller

--- client/src/MVC/controller/home/HomeController.tsx
+++ client/src/MVC/controller/home/HomeController.tsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import { WeatherModel, UserModel } from "../../model/home/HomeModel";
 import { HomeView } from "../../view/page";
 
 export default function HomeController() {
-  return <HomeView />;
+  const [userModel] = useState(new UserModel(123));
+  const [weatherModel] = useState(new WeatherModel());
+
+  const [user, setUser] = useState(null);
+  const [weather, setWeather] = useState(null);
+  const [weatherIcon, setWeatherIcon] = useState(null);
+
+  useEffect(() => {
+    userModel.fetchDemoUser().then(() => {
+      return setUser(userModel.getUser());
+    });
+    weatherModel
+      .fetchWeater()
+      .then(() => {
+        setWeather(weatherModel.getWeather()?.weather[0]?.main);
+        setWeatherIcon(weatherModel.getWeather()?.weather[0]?.icon);
+      })
+      .catch((res) => setWeather(res));
+  }, [user, userModel, weatherModel]);
+
+  console.log(weather);
+
+  return <HomeView weather={weather} weatherIcon={weatherIcon} />;
 }
